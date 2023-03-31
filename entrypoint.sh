038b4ccc0108051d9c11c61d3f267baf68760343
Tonye Jack <jtonye@ymail.com>
2023-03-30 23:44:05 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-sed 's@REPOSITORY_URL@'"$INPUT_REPOSITORY_URL"'@g' cliff-template.toml > cliff.toml
+TMP_DIR=$(mktemp -d)
+OUTPUT_PATH="$TMP_DIR/cliff.toml"
+
+sed 's@REPOSITORY_URL@'"$INPUT_REPOSITORY_URL"'@g' cliff-template.toml > "$OUTPUT_PATH"
+
+echo "output_path=$OUTPUT_PATH" >> "$GITHUB_OUTPUT"
