b59492be51e450534cf97453d9eeda943ac25895
Tonye Jack <jtonye@ymail.com>
2023-03-31 00:16:34 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 OUTPUT_PATH="cliff.toml"
 
-if [[ ! -f "$OUTPUT_PATH" ]];
+if [[ ! -f "$OUTPUT_PATH" ]]; then
   sed 's@REPOSITORY_URL@'"$INPUT_REPOSITORY_URL"'@g' cliff-template.toml > "$OUTPUT_PATH"
   
   echo "output_path=$OUTPUT_PATH" >> "$GITHUB_OUTPUT"
